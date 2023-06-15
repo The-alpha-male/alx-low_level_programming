eeb6064dd955d27caa114ba8026079d938599aa9
The-alpha-male <ronnymunene@outlook.com>
2023-06-15 09:15:30 -0700

    prints programming is a puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
 #!/bin/bash
-int main() { puts("Programming is like building a multilingual puzzle\n"); return 0; }
+#include <stdio.h>
+
+/**
+ * main - This is the main entry point of our function
+ * The function prints a specific message
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
