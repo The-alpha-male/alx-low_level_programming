c31df613d8f7f74a0e4569715fbdcfb3de59e42d
The-alpha-male <ronnymunene@outlook.com>
2023-06-15 14:55:14 -0700

    print using write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - entry point
- *
- * return : always 1 (success)
- */
+* main - entry point
+*
+* Return: always 1 (Success)
+*/
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
