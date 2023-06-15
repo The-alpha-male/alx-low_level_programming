45c70cebd30a287d9c11c1106d0866cf3b30288f
The-alpha-male <ronnymunene@outlook.com>
2023-06-15 12:26:50 -0700

    prints the size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - This is the main entry point of our function
- * The function prints the size of various types on the computer it is compiled 
+ * main - Entry point
  *
- * Return: 0 if successful
+ * Return: Always 0 (Success)
  */
-
-int main()
+int main(void)
 {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of short: %zu byte(s)\n", sizeof(short));
-printf("Size of long: %zu byte(s)\n", sizeof(long));
-printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-printf("Size of unsigned int: %zu byte(s)\n", sizeof(unsigned int));
-printf("Size of unsigned long: %zu byte(s)\n", sizeof(unsigned long));
-printf("Size of unsigned long long: %zu byte(s)\n", sizeof(unsigned long long));
-printf("Size of void*: %zu byte(s)\n", sizeof(void*));
-return 0;
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
+	return (0);
 }
+
