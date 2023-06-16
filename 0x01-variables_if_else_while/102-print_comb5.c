03d825b97dca30338f21b86a5542945c2376ae94
The-alpha-male <ronnymunene@outlook.com>
2023-06-16 15:56:56 -0700

    prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers
+ *
+ * Description: This program generates and prints all possible combinations of two digits 0 to 99 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, j;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (x < j)
+			{
+				putchar((x / 10) + '0');
+				putchar((x % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (x != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
