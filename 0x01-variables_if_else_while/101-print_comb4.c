c7ce8ce77e0f6b48ec2098f77266964413beb741
The-alpha-male <ronnymunene@outlook.com>
2023-06-16 15:44:35 -0700

    prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,9 +14,9 @@ int digit_1, digit_2, digit_3;
 /* Loop to print the combinations */
 for (digit_1 = 0; digit_1 <= 7; digit_1++)
 {
-for (digit2 = digit_1 + 1; digit_2 <= 8; digit_2++)
+for (digit_2 = digit_1 + 1; digit_2 <= 8; digit_2++)
 {
-for (digit3 = digit_2 + 1; digit_3 <= 9; digit_3++)
+for (digit_3 = digit_2 + 1; digit_3 <= 9; digit_3++)
 {
 /* Print the current combination */
 putchar(digit_1 + '0');
