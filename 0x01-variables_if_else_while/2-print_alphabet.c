b1344e5c2afcbcea3ed0877ec879cb8590195d34
The-alpha-male <ronnymunene@outlook.com>
2023-06-16 13:11:39 -0700

    prints lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lowercase
+ * return : 0 (success)
+ */
+int main(void)
+{
+	/* initialize char */
+	char alphabets;
+	/* for loop for a-z*/
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+1-last_digit assigns a raandom number to var n and prints last digit in the var
