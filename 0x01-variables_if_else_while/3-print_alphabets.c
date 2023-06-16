803f4350d5d78cbf2490dfbaf2d9c0e30987757c
The-alpha-male <ronnymunene@outlook.com>
2023-06-16 13:54:14 -0700

    prints lowercase and uppercase alphabets using putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	/* initialize char */
+
 	char l_alphabet, up_alphabet;
-	/* lowercase for loop */
+
 	for (l_alphabet = 'a'; l_alphabet <='z'; l_alphabet ++)
 		putchar(l_alphabet);
 	
-	/* uppercase loop */
+
 	for (up_alphabet = 'A'; up_alphabet <= 'Z'; up_alphabet ++)
 		putchar(up_alphabet);
 
