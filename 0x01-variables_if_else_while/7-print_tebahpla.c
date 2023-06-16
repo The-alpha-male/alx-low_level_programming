e24e8a002d63fdac86fb5e16a98b3c65db0af9dd
The-alpha-male <ronnymunene@outlook.com>
2023-06-16 14:49:01 -0700

    prints z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <sdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char reverse_alphabet;
+	for (reverse_alphabet = 'z';reverse_alphabet >= 'a'; reverse_alphabet --)
+		putchar(reverse_alphabet);
+	putchar('\n');
+	return (0);
+}
