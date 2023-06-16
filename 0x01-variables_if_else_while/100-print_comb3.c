2e6359400ca72512f84af88483e7dcd8a27cc43f
The-alpha-male <ronnymunene@outlook.com>
2023-06-16 15:30:57 -0700

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry point
- * Return:0 (success)
-*/
+ *
+ * Description: Prints all possible different combinations of two digits,
+ *              separated by ", ", in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int digit_1, digit_2;
 
-	for (digit_1 = '0'; digit_1 < '9'; digit_1++)
+	for (digit_1 = 0; digit_1 <= 8; digit_1++)
 	{
-		for (digit_2 = digit_1 + 1; digit_2 <= '9'; digit_2++)
+		for (digit_2 = digit+1 + 1; digit_2 <= 9; digit_2++)
 		{
-			if (digit_2 != digit_1)
+			putchar(digit_1 + '0');
+
+			putchar(digit_2 + '0');
+
+			if (digit_1 != 8 || digit_2 != 9)
 			{
-				putchar(digit_1);
-				putchar(digit_2);
-				if (digit_1 == '0' && digit_2 == '9')
-					continue;
 				putchar(',');
 				putchar(' ');
+
 			}
 		}
 	}
