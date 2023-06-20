499c6afeec687eb2db6204e8e57322eb1502ccb4
The-alpha-male <ronnymunene@outlook.com>
2023-06-20 14:44:26 -0700

    computes and prints the sum of all the multiples of 3 or 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,45 +1,28 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
-  */
+ * main - Entry point
+ *
+ * Description: Computes and prints the sum of all
+ * the multiples of 3 or 5 below 1024.
+ *
+ * Return: Always 0 (Success)
+ */
 
-void print_times_table(int n)
+int main(void)
 {
-	int num, mult, prod;
+	int sum = 0;
+	int j;
 
-	if (n >= 0 && n <= 15)
+	/* iterate over the number from 0 to 1023 */
+	for (j = 0; j < 1024; j++)
 	{
-		for (num = 0; num <= n; num++)
+		if (j % 3 == 0 || j % 5 == 0)
 		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
+			sum += j;
 		}
 	}
+	printf("%d\n", sum);
+
+	return (0);
 }
