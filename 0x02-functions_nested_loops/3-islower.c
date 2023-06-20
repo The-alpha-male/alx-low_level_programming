22f86839acbc1ea9cec4f29261323d0aceec35e4
The-alpha-male <ronnymunene@outlook.com>
2023-06-20 07:38:19 -0700

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Main - checks for lowercase character
+ * Return - 0(success)
+ */
+
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
