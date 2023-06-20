7315cd90b2e3810007e870be0982e3516df45597
The-alpha-male <ronnymunene@outlook.com>
2023-06-20 07:21:52 -0700

    print lowercase 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase followed by a new line
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char lower_case;
