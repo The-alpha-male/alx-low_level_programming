da589a94e75babf99e068499c8f63cfa60c96304
The-alpha-male <ronnymunene@outlook.com>
2023-06-20 06:01:51 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,50 +1,21 @@
-/*
- * print_putchar.c
- *
- * This program prints the string "_putchar" followed by a new line.
- *
- * Usage:
- *   ./print_putchar
- *
- * Author: The-alpha-male
- */
-
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Prints the string "_putchar" followed by a new line.
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
-int main(void) {
-  /* Prints the character '_'. */
-  _putchar('_');
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-  /* Prints the character 'p'. */
-  _putchar('p');
-
-  /* Prints the character 'u'. */
-  _putchar('u');
-
-  /* Prints the character 't'. */
-  _putchar('t');
-
-  /* Prints the character 'c'. */
-  _putchar('c');
-
-  /* Prints the character 'h'. */
-  _putchar('h');
-
-  /* Prints the character 'a'. */
-  _putchar('a');
-
-  /* Prints the character 'r'. */
-  _putchar('r');
-
-  /* Prints a new line. */
-  putchar('\n');
-
-  /* Returns 0 to indicate success. */
-  return 0;
+	return (0);
 }
