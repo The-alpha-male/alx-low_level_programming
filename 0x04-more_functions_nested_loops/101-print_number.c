6635341dd31752e3b7325b6b68df7fdbb74c37dd
The-alpha-male <ronnymunene@outlook.com>
2023-06-22 08:17:53 -0700

    prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_integer - Prints an integer.
+ * @num: The integer to be printed.
+ *
+ * Return: void
+ */
+
+void print_integer(int num)
+{
+	printf("%d\n", num);
+}
