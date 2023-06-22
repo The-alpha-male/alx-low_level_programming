63dc3055a14b8457b980065a9f00fb5d422a65e2
The-alpha-male <ronnymunene@outlook.com>
2023-06-22 06:20:59 -0700

    prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- print number
+ *
+ * Return: The product of a and b
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
