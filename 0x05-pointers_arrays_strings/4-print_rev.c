4253fba45a77534dc5bef96c88695b5d95cbcbc3
The-alpha-male <ronnymunene@outlook.com>
2023-06-25 21:51:19 -0700

    prints a string in reverse followed by new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,11 +5,23 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	char *str;
 
-	str = "Doing hard things in alx";
-	print_rev(str);
-	return (0);
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (j = i; j > 0; j--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
 }
