50b7f1c92afe47c883163f032350f7c6f83e169a
The-alpha-male <ronnymunene@outlook.com>
2023-07-05 09:41:52 -0700

    function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,52 @@
 #include "main.h"
 
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to check.
- *
- * Return: 1 if @n is prime, 0 otherwise.
- */
+  *evaluate_num - recursion loop
+  *@num: num
+  *@iterator: number to iterate
+  *Return: return 1 or 0
+  */
 
+int evaluate_num(int num, int iterator)
+{
+		if (iterator == num - 1)
+		{
+			return (1);
+		}
+
+		else if (num % iterator == 0)
+		{
+			return (0);
+		}
+
+		if (num % iterator != 0)
+		{
+			return (evaluate_num(num, iterator + 1));
+		}
+
+		return (0);
+}
+/**
+   * is_prime_number - evaluate prime or not
+   * @n: number
+   *Return: return 1 prime - return 0 otherwise
+   */
 int is_prime_number(int n)
 {
-	int i;
+		int iterator;
 
-	if (n <= 1)
-		return (0);
+			iterator = 2;
 
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+				/* only greater than 2*/
+				if (n < 2)
+				{
+					return (0);
+				}
 
-	return (1);
+				if (n == 2)
+				{
+					return (1);
+				}
+
+				return (evaluate_num(n, iterator));
 }
