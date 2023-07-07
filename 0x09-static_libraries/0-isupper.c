ff86e86b2263631c5143c651f786293ab2851d17
The-alpha-male <ronnymunene@outlook.com>
2023-07-07 08:49:09 -0700

    static library

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 Success
  */
 
 int _isupper(int c)
@@ -12,5 +11,5 @@ int _isupper(int c)
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
-	return (0);
+		return (0);
 }
--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
- /**
+/**
  *_strcat - concatenates two strings
  *@dest: input value
  *@src: input value
- *
- *Return: void	
+ * Return: void
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -25,8 +23,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
-
+	}
 
 	dest[i] = '\0';
 	return (dest);
