00f59ed6438313002269d3a8b7fe5c8d4a1376c5
The-alpha-male <ronnymunene@outlook.com>
2023-07-19 11:50:04 -0700

    error fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,15 +1,12 @@
 #ifndef CALC_H
 #define CALC_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
  * struct op - Struct op
  * @op: operator
  * @f: function associated
  */
+
 typedef struct op
 {
 	char *op;
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,48 +1,39 @@
-#include <stdio.h>
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success)
+ * main - print the result of a operation
+ * @argc: int
+ * @argv: char pointer of pointer
+ * Return: 0 always
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int (*func)(int, int), ans, v1, v2;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
 
-	if (!func)
+	if (func == NULL)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(99);
 	}
 
-	o = *argv[2];
+	v1 = atoi(argv[1]);
+	v2 = atoi(argv[3]);
 
-	if ((o == '/' || o == '%') && arg2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+	ans = func(v1, v2);
 
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
+	printf("%d\n", ans);
 
 	return (0);
 }
