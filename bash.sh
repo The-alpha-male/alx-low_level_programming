a7e65c404975e04661792ad59c16850eef57cd31
The-alpha-male <ronnymunene@outlook.com>
2024-02-22 05:35:44 -0800

    This is an automated git command using bash

--- bash.sh
+++ bash.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-#echo "$"
-echo $(git add . ; git commit -m '$2' ; git push)
+# Ask user for commit message
+echo "Enter commit message:"
+read commit_message
+
+# Add all changes
+git add .
+
+# Commit changes with custom message
+git commit -m "$commit_message"
+
+# Push changes
+git push
